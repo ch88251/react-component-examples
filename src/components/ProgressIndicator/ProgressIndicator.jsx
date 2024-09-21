fe100e5b82c6172f9d65e67d055412b071370629
Charles Hayes <cfhayes16@gmail.com>
2024-09-20 19:23:51 -0600

    created html for component

--- src/components/ProgressIndicator/ProgressIndicator.jsx
+++ src/components/ProgressIndicator/ProgressIndicator.jsx
@@ -2,11 +2,22 @@ import React, { useEffect, useState } from 'react';
 import './style.css';
 
 const ProgressIndicator = () => {
-  return (
-    <div>
-      <h3>Progress Indicator</h3>
-    </div>
-  );
+    return (
+        <div>
+            <h3>Progress Indicator</h3>
+            <div className="container">
+                <div className="progress-container">
+                    <div className="progress" id="progress"></div>
+                    <div className="circle active">1</div>
+                    <div className="circle">2</div>
+                    <div className="circle">3</div>
+                    <div className="circle">4</div>
+                </div>
+                <button className="btn" id="prev" disabled>Previous</button>
+                <button className="btn" id="next">Next</button>
+            </div>
+        </div>
+    );
 }
 
 export default ProgressIndicator;
