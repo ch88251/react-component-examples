f1b6fcd1c767b44106a19f1e42d44b65f69fcc03
Charles Hayes <cfhayes16@gmail.com>
2024-09-20 19:15:39 -0600

    Added styling

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ProgressIndicator from './components/ProgressIndicator/ProgressIndicator';
+
+function App() {
+    return (
+        <div className="App">
+            <ProgressIndicator/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/ProgressIndicator/ProgressIndicator.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import './style.css';
+
+const ProgressIndicator = () => {
+  return (
+    <div>
+      <h3>Progress Indicator</h3>
+    </div>
+  );
+}
+
+export default ProgressIndicator;
