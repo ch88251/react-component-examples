bb63a214cda21734d29c56963d283ee3adfe6e66
Charles Hayes <cfhayes16@gmail.com>
2024-09-20 21:16:01 -0600

    fixed bug

--- src/components/ProgressIndicator/ProgressIndicator.jsx
+++ src/components/ProgressIndicator/ProgressIndicator.jsx
@@ -8,7 +8,7 @@ const ProgressIndicator = () => {
         const progress = document.getElementById('progress');
         const prev = document.getElementById('prev');
         const next = document.getElementById('next');
-        const circles = document.querySelectorAll('circle');
+        const circles = document.querySelectorAll('.circle');
 
         let currentActive = 1;
 
