e858ff751883fed7f92fc23f7d909e7dbb08c19c
Charles Hayes <cfhayes16@gmail.com>
2024-09-20 21:06:30 -0600

    added javascript for button event handling

--- src/components/ProgressIndicator/ProgressIndicator.jsx
+++ src/components/ProgressIndicator/ProgressIndicator.jsx
@@ -2,10 +2,61 @@ import React, { useEffect, useState } from 'react';
 import './style.css';
 
 const ProgressIndicator = () => {
+    const [currentActive, setCurrentActive] = useState(1);
+
+    useEffect(() => {
+        const progress = document.getElementById('progress');
+        const prev = document.getElementById('prev');
+        const next = document.getElementById('next');
+        const circles = document.querySelectorAll('circle');
+
+        let currentActive = 1;
+
+        next.addEventListener('click', () => {
+            currentActive++;
+            if(currentActive > circles.length) {
+                currentActive = circles.length;
+            }
+            update();
+        });
+
+        prev.addEventListener('click', () => {
+            currentActive--;
+            if(currentActive < 1) {
+                currentActive = 1;
+            }
+            update();
+        });
+
+        function update() {
+            circles.forEach((circle, idx) => {
+                if(idx < currentActive) {
+                    circle.classList.add('active');
+                } else {
+                    circle.classList.remove('active');
+                }
+            });
+
+            const actives = document.querySelectorAll('.active');
+
+            progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%';
+
+            if(currentActive === 1) {
+                prev.disabled = true;
+            } else if (currentActive === circles.length) {
+                next.disabled = true;
+            } else {
+                prev.disabled = false;
+                next.disabled = false;
+            }
+        }
+
+    }, [currentActive]);
+
     return (
         <div>
-            <h3>Progress Indicator</h3>
             <div className="container">
+                <h3>Progress Indicator</h3>
                 <div className="progress-container">
                     <div className="progress" id="progress"></div>
                     <div className="circle active">1</div>
